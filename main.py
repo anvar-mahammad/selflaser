8c9d3cc47cdd46ab5616bc945c7d03e4a64bfded
anvar-mahammad <anvar.mahammad@gmail.com>
2024-02-16 23:07:26 +0000

    image size

--- main.py
+++ main.py
@@ -10,10 +10,21 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
-col1, col2, col3 = st.columns([1,2,1])
-
-with col2:  # Use the middle column for your image
-    st.image("self.png", use_column_width=True,width=100)
+st.markdown("""
+    <style>
+    .container {
+        display: flex;
+        justify-content: center;
+    }
+    .image {
+        max-width: 100%;
+        height: auto;
+    }
+    </style>
+    <div class="container">
+        <img class="image" src="self.png"/>
+    </div>
+    """, unsafe_allow_html=True)
 
 def column_checkbox_maker(col_list, col_length, check_dict):
     print("run")
